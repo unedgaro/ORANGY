a607a3baa43f885a7d5dae377c1dd22ef2ec761e
unedgaro <unedgaro123@hotmail.com>
2019-05-03 20:05:36 -0500

    estructura de la pagina

---
+++ DisenosAlfa/resources/js/Prueba1.js
@@ -0,0 +1,9 @@
+
+//$(document).ready(function(){
+  //$('.divcontenido').mouseover(function(){
+   //$('.divcontenido').css("background","gray");
+  //});
+function muestraImg(tipo,elem){
+	document.getElementById('vinil'+elem).css = "visibility:visible;";
+}
+ 
